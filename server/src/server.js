f6dc8b7cd86ecaac3badea081a9869398da24785
alecsandernst <naama.stavares@gmail.com>
2026-09-15 10:54:09 -0300

    feat: implementando rota de criação de usuário na tabela

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,29 @@
-const express = require('express');
 require("dotenv").config();
+const express = require('express');
+const supabase = require('./config/supabase');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('API Online!');
+});
+
+app.post('/usuarios', async (req, res) => {
+  const {email, nome, senha} = req.body;
+  try {
+
+    await supabase.from('usuarios')
+    .insert([{ email, nome, senha }]);
+    res.status(201).json({ message: 'Usuário criado com sucesso!' });
+  } catch (error) {
+
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao criar usuário.' });
+  }
+
+
 });
 
 app.listen(port, () => {

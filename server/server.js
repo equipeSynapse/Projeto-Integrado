3a98f1e5ee868ec9b42f6e52da9e879c037ae710
Anderson Maia <124885820+TheAnders007@users.noreply.github.com>
2026-09-19 19:31:21 -0300

    Reestrutura as pastas dentro de src

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./src/routes/auth.routes.js";
 
 const app = express();
 const port = 3000;

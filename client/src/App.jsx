4dfcbcf8080a44633fd31055fd7ce2bfc4cfd781
alecsandernst <naama.stavares@gmail.com>
2026-09-25 11:52:52 -0300

    feat: inicializando frontend de cadastro e configuração de tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-5xl font-bold text-blue-500">
+          Tailwind funcionando!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
